078ccef2831c13ca34e81be42b1963779bf8284f
Junaid Atari <mj.atari@gmail.com>
2022-07-02 14:00:51 +0500

    fix typescript build issues, adjust babel config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,12 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
-    'no-console': 'off',
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   }
 };
